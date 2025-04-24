5b7f19e688066d1596289b12bc4176c08c7d18b7
Faruk Hossain <hossainf114@gmail.com>
2025-04-24 16:52:02 +0530

    Precommit Fix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,4 +6,11 @@ from accounts.models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["id", "username", "email", "is_admin", "is_staff", "is_editor"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "is_admin",
+            "is_staff",
+            "is_editor",
+        ]
--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,14 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from accounts.urls import router as accounts_router
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,17 +39,29 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+router = DefaultRouter()
+router.registry.extend(accounts_router.registry)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path(
+        "swagger.json",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/accounts/", include("accounts.urls")),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path("", include(router.urls), name="api"),
 ]
